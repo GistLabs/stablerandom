d6782fffc366be4d1f6d057af23b8a44fe112d80
John Heintz <john@gistlabs.com>
2023-02-06 16:30:43 -0600

    build basic structure and create a failing unit test

--- stablerandom/stablerandom.py
+++ stablerandom/stablerandom.py
@@ -1,2 +1,47 @@
 # Copyright (c) 2023 John Heintz, Gist Labs https://gistlabs.com
 # License Apache v2 http://www.apache.org/licenses/
+
+
+from numpy.random import Generator, PCG64
+import threading
+
+
+class RandomLocal(threading.local):
+    '''A threadlocal to hold a stack of stable randome generators'''
+    stack = []
+
+    def push(self):
+        '''Push a new Generator PCG64 SeedSequence to the stack'''
+        self.stack.append(Generator(PCG64(123456789)))
+
+    def pop(self):
+        '''Removes the top element from the stack'''
+        self.stack.pop(0)
+
+    def top(self):
+        '''Lookup the top random generator, or return None'''
+        if len(self.stack) > 0:
+            return self.stack[-1]
+        else:
+            return None
+
+_randomLocalStack = RandomLocal()
+
+def stablerandom(func):
+    """@stablerandom can be used stable random behavior for a function and call stack
+
+    Usage:
+        @stable_random
+        def my_method():
+            ...
+    """
+    def wrapper(*args, **kwargs):
+        _randomLocalStack.push()
+
+        try:
+            return func(*args, **kwargs)
+        finally:
+            # clear thread local
+            _randomLocalStack.pop()
+
+    return wrapper
--- stablerandom/stablerandom_test.py
+++ stablerandom/stablerandom_test.py
@@ -1,2 +1,21 @@
 # Copyright (c) 2023 John Heintz, Gist Labs https://gistlabs.com
 # License Apache v2 http://www.apache.org/licenses/
+
+import numpy.random
+import pytest
+from stablerandom import stablerandom
+from stablerandom.stablerandom import _randomLocalStack
+
+def random():
+    return numpy.random.triangular(1,5,10)
+
+@stablerandom
+def stable():
+    return numpy.random.triangular(1, 5, 10)
+
+@stablerandom
+def nested():
+    return stable(), stable(), random()
+
+def test_stable_is_stable():
+    assert stable() == stable()
