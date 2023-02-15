33b7a353f9eab53f70f550d3c176ae0f6fb24d43
Gabriel Omagwu <89912584+gomagwu@users.noreply.github.com>
2023-02-16 00:38:55 +0100

    added the publish commands to "ci.yml"
    - Defined the version on the setup.cfg
    - Setup version on init
    -

--- stablerandom/__init__.py
+++ stablerandom/__init__.py
@@ -1,6 +1,12 @@
 # Copyright (c) 2023 John Heintz, Gist Labs https://gistlabs.com
 # License Apache v2 http://www.apache.org/licenses/
-
+import os
 from .stablerandom import *
 
-a = 1 + 2
+__version__ = "0.1.18"
+
+path = os.getcwd()
+if os.path.exists(f'{path}\\__build_version.py') is True:
+    with open('__build_version.py') as f:
+        lines = f.read()
+    __version__ = f"{__version__}.{lines.strip()}"
