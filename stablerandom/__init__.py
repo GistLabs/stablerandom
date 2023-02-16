05d5eabf80de8cff260a439f0a12f945b5522d7a
John Heintz <john@gistlabs.com>
2023-02-16 14:48:29 -0600

    Move to import with python variable in __build_version.py echo rev count and short hash in build process

--- stablerandom/__init__.py
+++ stablerandom/__init__.py
@@ -1,12 +1,10 @@
 # Copyright (c) 2023 John Heintz, Gist Labs https://gistlabs.com
 # License Apache v2 http://www.apache.org/licenses/
-import os
 from .stablerandom import *
 
-__version__ = "0.1.18"
-
-path = os.getcwd()
-if os.path.exists(f'{path}\\__build_version.py') is True:
-    with open('__build_version.py') as f:
-        lines = f.read()
-    __version__ = f"{__version__}.{lines.strip()}"
+__version__ = "0.1"
+try:
+    import stablerandom.__build_version
+    __version__ = f'{__version__}.{stablerandom.__build_version.buildVersion}'
+except ImportError:
+    pass
